163ebf1bf7d4559e24bd14ddf1a003bf4ccc7b89
Tobias K <6317548+theCalcaholic@users.noreply.github.com>
2021-08-23 23:31:33 +0200

    inject_iframe.js: Add Info button

--- js/inject_iframe.js
+++ js/inject_iframe.js
@@ -1,12 +1,31 @@
 (function() {
+
+  let co2clock_container = document.createElement("div");
+  co2clock_container.style.width = "100%";
+  co2clock_container.style.display = "block";
+  co2clock_container.style.marginTop = "2em";
+  co2clock_container.style.marginBottom = "2em";
+  co2clock_container.style.textAlign = "center";
+
   let frame = document.createElement("iframe");
   frame.setAttribute("src", "https://www.mcc-berlin.net/fileadmin/data/clock/carbon_clock.htm?i=3267263");
   frame.style.width = "700px";
   frame.style.height = "450px";
-  frame.style.marginTop = "2em";
-  frame.style.marginBottom = "2em";
+  frame.style.marginBottom = "4px";
 
-  document.querySelector(".wrapper .v-align").appendChild(frame);
+
+  let info_link = document.createElement("a");
+  info_link.innerText = "Info";
+  info_link.href = "https://www.mcc-berlin.net/en/research/co2-budget.html";
+  info_link.style.display = "block";
+  info_link.style.width = "700px";
+  info_link.style.backgroundColor = "black";
+
+  co2clock_container.appendChild(frame);
+  co2clock_container.appendChild(info_link);
+
+  let wrapper = document.querySelector(".wrapper .v-align");
+  wrapper.appendChild(co2clock_container);
 
   let zoom = (window.screen.availWidth - 10) /frame.clientWidth;
   //console.log(`window width: ${window.screen.availWidth}, frame width: ${frame.clientWidth}|${frame.offsetWidth}, zoom: ${zoom}`);
@@ -15,6 +34,10 @@
     frame.style.transform = `scale(${zoom})`;
     let frameMargin = Math.floor((window.screen.availWidth - (frame.clientWidth * zoom)));
     frame.style.transformOrigin = `${frameMargin}px 0`;
+    info_link.style.width = `${Math.floor(frame.clientWidth * zoom)}px`;
+    info_link.style.marginTop = `${Math.floor(frame.clientHeight * (zoom - 1))}px`;
+    info_link.style.marginLeft = "auto";
+    info_link.style.marginRight = "auto";
   }
 
 })()
